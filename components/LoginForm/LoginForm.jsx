6c39ad7c1d65ea5e784df153880f07e7a1827a55
Бонифаций <142723667+v0idhrt@users.noreply.github.com>
2024-12-07 17:14:05 +0300

    Delete components directory

--- components/LoginForm/LoginForm.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "LoginForm.module.css";
-
-const LoginForm = () => {
-    <section className={styles.LoginSection}>
-        <h1 className={styles.HeaderForm}></h1>
-    </section>
-}
